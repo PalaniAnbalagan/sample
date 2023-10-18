d2fd28b6d342ae95aa811e7b3bcb6fdb918ab949
Palani <palani.aj@gmail.com>
2023-10-18 16:21:59 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Sample page</h1>
+      <h1 className='text-3xl font-bold underline'>Welcome to Sample page</h1>
     </div>
   );
 }
